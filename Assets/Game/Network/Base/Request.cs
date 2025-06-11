d3a9f68c15ec37066f23e74554533bc2448c740a
LunarEclipse <949886@qq.com>
2025-06-11 15:09:54 +0800

    ssid debug

--- Assets/Game/Network/Base/Request.cs
+++ Assets/Game/Network/Base/Request.cs
@@ -26,13 +26,8 @@ namespace USEN
                 },
             };
             
-#if !DEBUG && UNITY_ANDROID
             var ssid = USEN.AndroidPreferences.Ssid ?? "01HA1S5FCXKDB78KGBZ0QP3HYQ";
             var tvIdentifier = USEN.AndroidPreferences.TVIdentifier ?? "N00000000000000065760";
-#else
-            var ssid = "01HA1S5FCXKDB78KGBZ0QP3HYQ";
-            var tvIdentifier = "N00000000000000065760";
-#endif
 
             
             Debug.Log($"Request with ssid={ssid}, tvIdentifier={tvIdentifier}");
